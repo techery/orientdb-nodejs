cabd472ff527cc50fd1f3a8cacd3e4794fe51650
Nikita Galkin <nikita@galk.in>
2016-01-12 20:16:37 +0200

    Add new settings

--- app/services/settings.js
+++ app/services/settings.js
@@ -4,9 +4,15 @@ exports = module.exports = function () {
     return {
         appName: _getEnv('ENV_NAME', 'OrintDBPerfomanceTester'),
         environment: _getEnv('NODE_ENV', 'local'),
+        node:{
+            id:_getEnv('NODE_ID', 'local'),
+            count:_getEnv('NODE_COUNT', 'local'),
+            minUserCount:_getEnv('NODE_MIN_USER_COUNT', 'local'),
+        },
         port: _getEnv('ENV_PORT', 3000),
         workerCount: _getEnv('NUMBER_OF_WORKERS', 2),
         intervalOfWorkerCheck: _getEnv('INTERVAL_OF_WORKER_CHECK_MS', 5000),
+        userPerWorker: _getEnv('USERS_PER_WORKER', 100),
         dbParams: {
             host: _getEnv('DB_HOST', 'localhost'),
             port: _getEnv('DB_PORT', 2424),
@@ -15,6 +21,17 @@ exports = module.exports = function () {
         },
         dbName: _getEnv('DB_DATABASE', 'OrintDBPerfomanceTest'),
         dbClassName: _getEnv('DB_USER_CLASS', 'User'),
+        userInfo: {
+            itemPerFeed: _getEnv('USER_ITEM_PER_FEED', 50),
+            chanceLike: _getEnv('USER_CHANCE_LIKE', 0.02),
+            chanceRead: _getEnv('USER_CHANCE_READ', 0.02),
+            chanceComment: _getEnv('USER_CHANCE_COMMENT', 0.005),
+            activityChanceRemoveFriendship: _getEnv('USER_ACTIVITY_TIMEOUT', 300),
+            activityChanceNewFriendship: _getEnv('USER_ACTIVITY_CHANCE_REMOVE_FRIENDSHIP', 0.05),
+            activityChanceWritePost: _getEnv('USER_ACTIVITY_CHANCE_NEW_FRIENDSHIP', 0.15),
+            activityChanceNothing: _getEnv('USER_ACTIVITY_CHANCE_WRITE_POST', 0.8),
+            timeoutActivity: _getEnv('USER_ACTIVITY_CHANCE_NOTHING', 0),
+        },
     };
 };
 
