3b3738d7a372b14bc253794a4714b0c8f539545d
Nikita Galkin <nikita@galk.in>
2016-01-16 15:43:21 +0200

    Workers off

--- app/services/settings.js
+++ app/services/settings.js
@@ -12,7 +12,7 @@ exports = module.exports = function () {
             minUserCount: parseInt(_getEnv('NODE_MIN_USER_COUNT', 600000)),
         },
         port: _getEnv('ENV_PORT', 3000),
-        workerCount: _getEnv('NUMBER_OF_WORKERS', require('os').cpus().length),
+        workerCount: 0, //_getEnv('NUMBER_OF_WORKERS', require('os').cpus().length),
         intervalOfWorkerCheck: _getEnv('INTERVAL_OF_WORKER_CHECK_MS', 1000),
         userPerWorker: _getEnv('USERS_PER_WORKER', 100),
         dbParams: {
