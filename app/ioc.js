80b8589451d6f1b3cf7f39f72c0e7b4753e4a50c
Nikita Galkin <nikita@galk.in>
2016-01-21 17:01:46 +0200

    Update reporter

--- app/ioc.js
+++ app/ioc.js
@@ -6,10 +6,11 @@ IoC.use(IoC.node_modules('bunyan'));
 IoC.use(IoC.node_modules('orientjs'));
 IoC.use(IoC.node_modules('datadog-metrics'));
 IoC.use(IoC.node_modules('os'));
+IoC.use(IoC.node_modules('events'));
+IoC.use(IoC.node_modules('lorem-ipsum'));
 IoC.use(IoC.node('app/services'));
 IoC.use(IoC.node('app/core'));
 IoC.use(IoC.node('app/workers'));
 IoC.use(IoC.node('app/repositories'));
-IoC.use(IoC.node('lorem-ipsum'));
 
 module.exports = IoC;
