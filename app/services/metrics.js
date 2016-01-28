efc32b39ade00c4e8c9a0d6418069d9b05bdb824
Nikita Galkin <nikita@galk.in>
2016-01-28 15:00:04 +0200

    Rename datadog

--- app/services/metrics.js
+++ app/services/metrics.js
@@ -4,7 +4,7 @@ exports = module.exports = function(datadog, settings, os) {
   return new datadog.BufferedMetricsLogger({
     apiKey: settings.dataDogApiKey,
     host: os.hostname,
-    prefix: `galkin_test.`, //TODO: change techery_test
+    prefix: `orientDB_perfomance.`,
     flushIntervalSeconds: 0,
   });
 };
