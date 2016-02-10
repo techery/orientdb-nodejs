f2aefd3a67aba872f8315fdf8d382484a9dea2d5
Fedor Petryk <fedor.p@techery.io>
2016-02-10 12:13:50 +0200

    Change random user bottom limit from 1 to 100

--- app/repositories/randomRepository.js
+++ app/repositories/randomRepository.js
@@ -84,7 +84,7 @@ function getRandomUser(chunk) {
       userId = chunk_400_600[Math.floor(Math.random()*chunk_400_600.length)];
       break;
     default:
-      userId = getRandomInt(1, 600000);
+      userId = getRandomInt(1000, 600000);
   }
   return `12:${userId}`;
 }
