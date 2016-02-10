6ef32765b0e9b7a53c0ad14925f3f8e74439d87e
Fedor Petryk <fedor.p@techery.io>
2016-02-10 09:41:10 +0200

    Add date&time of start and end display

--- bin/benchmark.sh
+++ bin/benchmark.sh
@@ -6,10 +6,15 @@ test_cases=$1
 while read test_case; do
   [ -z "$test_case" ] && continue
   echo "Starting test case"
+  now=$(date +"%m-%d-%Y %T")
+  echo "Start date : $now"
   echo ""
   $start_test $test_case
   echo ""
   echo "Test case ended"
+  now=$(date +"%m-%d-%Y %T")
+  echo "End date : $now"
+
   echo "Waiting for 5 minutes"
   sleep 5m
 done < "$test_cases"
